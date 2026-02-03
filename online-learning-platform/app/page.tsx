6ce648e7297a1ffd7526d26c5f7846177227110e
Mandla7784 <marsatti7784@gmail.com>
2026-02-03 13:23:00 +0000

    added drizzle kit for postgress database

--- online-learning-platform/app/page.tsx
+++ online-learning-platform/app/page.tsx
@@ -1,8 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 // import { FaShoppingCart } from 'react-icons/fa';
-
-
 
 
 export default function Home() {
@@ -10,6 +9,8 @@ export default function Home() {
     <div className="p-8">
       <h2 className="text-2xl font-bold mb-4">Hi</h2>
       <Button>Add To Cart</Button>
+      
+      <UserButton />
     </div>
             
   );
