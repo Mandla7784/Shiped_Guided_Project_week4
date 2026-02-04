89566cf2f5f7f8e7f255c3d1e052e30f86179c7e
Mandla7784 <marsatti7784@gmail.com>
2026-02-04 14:56:39 +0000

    updateds

--- online-learning-platform/app/layout.tsx
+++ online-learning-platform/app/layout.tsx
@@ -1,7 +1,12 @@
+"use Client";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Provider from "./provider";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +34,13 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
+
+          <Provider>
+            
+            {children}
+
+          </Provider>
+     
         </body>
       </html>
     </ClerkProvider>
