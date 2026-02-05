8ccd0d1e41aa27aafb9f737a9b500f151812451c
Mandla7784 <marsatti7784@gmail.com>
2026-02-05 10:26:04 +0000

    added logo svg

--- online-learning-platform/app/workspace/_components/AppSidebar.tsx
+++ online-learning-platform/app/workspace/_components/AppSidebar.tsx
@@ -6,15 +6,18 @@ import {
   SidebarGroup,
   SidebarHeader,
 } from "@/components/ui/sidebar"
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
  
 export default function AppSidebar() {
   return (
-  <Sidebar>
-      <SidebarHeader >
-        
+    <Sidebar>
+      <SidebarHeader className="p-4">
+        <Image src="/logo.svg" alt="logo" width={130} height={120} />
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup />
+        <Button>Create New Course</Button>
         <SidebarGroup />
       </SidebarContent>
       <SidebarFooter />
