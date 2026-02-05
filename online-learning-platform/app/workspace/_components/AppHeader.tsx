6a40946aacd92d9289b2f796ffb44d3991c81a45
Mandla7784 <marsatti7784@gmail.com>
2026-02-05 13:04:40 +0000

    updates on App header

--- online-learning-platform/app/workspace/_components/AppHeader.tsx
+++ online-learning-platform/app/workspace/_components/AppHeader.tsx
@@ -1,7 +1,16 @@
+import { User } from 'lucide-react'
 import React from 'react'
+
+import { SidebarTrigger } from '@/components/ui/sidebar'
+import { UserButton } from '@clerk/nextjs'
+
+
 
 export default function AppHeader() {
   return (
-    <div></div>
+    <div>
+        <SidebarTrigger/>
+        <UserButton/>
+    </div>
   )
 }
