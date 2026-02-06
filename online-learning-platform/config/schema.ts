022fd9a1465a069661bf13aef30ac784b32d967f
Mandla7784 <marsatti7784@gmail.com>
2026-02-06 06:10:59 +0000

    added a course table on schema file

--- online-learning-platform/config/schema.ts
+++ online-learning-platform/config/schema.ts
@@ -7,3 +7,11 @@ export const usersTable = pgTable("users", {
   email: varchar({ length: 255 }).notNull().unique(),
   subscribtion: varchar({ length: 255 }).notNull(),
 });
+
+
+
+const courseTable = pgTable("courses", {
+  id: integer("id").primaryKey().notNull(),
+  name: varchar("name", { length: 255 }).notNull(),
+  description: varchar("description", { length: 255 }).notNull(),
+});
