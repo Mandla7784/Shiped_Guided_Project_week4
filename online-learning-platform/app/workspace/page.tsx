985322d59b73a9dde788ac97ee5698965ed8ba4b
Mandla7784 <marsatti7784@gmail.com>
2026-02-06 07:16:54 +0000

    Fixed directory structur

--- online-learning-platform/app/workspace/page.tsx
+++ online-learning-platform/app/workspace/page.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import WelcomeBanner from './_components/WelcomeBanner'
 import CourseList from './_components/CourseList'
+import CourseDialog from './_components/CourseDialog'
 
 export default function WorkSpace() {
+  const [openDialog, setOpenDialog] = useState(false)
+
   return (
     <div>
-      <WelcomeBanner />
+      <WelcomeBanner onCreateCourse={() => setOpenDialog(true)} />
       <CourseList />
+      <CourseDialog open={openDialog} onOpenChange={setOpenDialog} />
     </div>
   )
 }
